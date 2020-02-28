b47a2cff81b7f58e8ab51629847ac7c1abafccf0
Oren Geva <oren@fdna.com>
2020-02-28 11:41:28 +0200

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -21,5 +25,67 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn two_results() {
+        let query = "Rust";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Rust.
+";
+
+        assert_eq!(
+            vec!["Rust:", "Rust."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn no_results() {
+        let query = "Ruwst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Rust.
+";
+
+        assert!(
+            search(query, contents).is_empty()
+        );
     }
 }
