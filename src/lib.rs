298da70f5032af30158a11fceff6845848609a3a
Oren Geva <oren@fdna.com>
2020-03-01 16:02:12 +0200

    Use iterators and closures

--- src/lib.rs
+++ src/lib.rs
@@ -24,45 +24,36 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // skip the program name arg
 
-        let case_sensitive = Config::resolve_case_sensitiveness(&args);
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = if env::var("CASE_INSENSITIVE").is_err() {
+            args.find(|s| s == &String::from("-i")).is_none()
+        } else {
+            false
+        };
 
         Ok(Config { query, filename, case_sensitive })
-    }
-
-    fn resolve_case_sensitiveness(args: &[String]) -> bool {
-        let mut case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-        // if env variable is not set, we'll check if param -i was passed
-        if case_sensitive {
-            for arg in args.into_iter() {
-                if arg == "-i" {
-                    case_sensitive = false;
-                }
-            }
-        }
-        case_sensitive
     }
 }
 
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
